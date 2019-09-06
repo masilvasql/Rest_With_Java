a01d5888a447ad62ec0d60fc0625b97f1b87e7fb
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-05 23:14:08 -0300

    conexao

--- src/java/Conexao/Conexao1Exemplo.java
+++ src/java/Conexao/Conexao1Exemplo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DAO;
+package Conexao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
