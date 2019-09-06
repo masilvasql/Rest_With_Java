d50a27cc559f57d392a09f92611e4739a2264292
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-09-05 21:17:05 -0300

    início do projeto

---
+++ src/java/ws/wsRest01.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author marce
+ */
+@Path("teste")
+public class wsRest01 {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of wsRest01
+     */
+    public wsRest01() {
+    }
+
+    /**
+     * Retrieves representation of an instance of ws.wsRest01
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        return "meu primeiro ws";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of wsRest01
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
